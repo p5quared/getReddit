c1e34faeab82ccec715d6bc816e297eac8de70fb
p5quared <petervaiciulis@gmail.com>
2022-08-21 15:09:44 -0400

    Refactored our main program to store Reddit content in dictionaries of ReddiObjects.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import os
+
 import praw
 from dotenv import load_dotenv
 from pprint import pprint
+
+from classes import RedditObject
 
 load_dotenv()
 USER_ID = os.getenv('USER_ID')
@@ -9,50 +12,48 @@ USER_PASSWORD = os.getenv('USER_PASSWORD')
 CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
-
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
     client_secret=CLIENT_SECRET,
     user_agent=f'python: PMAW request enrichment (by u/pto2)'
 )
 print(f'Reddit instance established?\n{reddit.read_only}\n')
-'''
-<< notes on PRAW >> 
-reddit.subreddit("abc")
-submissions
-    .title
-    .body
-    .score
-    .comments[n]
-     [n].body
-        .replies[n]
-         [n].body
-.replace_more(limit=0) >> eliminates "more comments"
-'''
-#  get title & top 3 comment chains (max 3 levels deep),
-#  from top post on subreddit
+
 saved_comments = list()
-submissions = reddit.subreddit('askreddit').hot(limit=1)
-with open("subtext.txt", "w") as f:
-    # TODO: cleanup usage of f.write()... append all to [[],[]] and use .writelines()
+submissions = reddit.subreddit('askreddit').hot(limit=3)
+gathered_content = list()
 
-    # TODO: remove print statements/differentiate whether to print output or write output
-    #       (eventually prints will almost certainly be eliminated)
+# Works for text based Q/A subs i.e. r/askreddit, r/askmen...
+for n, submission in enumerate(submissions):
+    post = {"submission": None, "comments": list()}  # container for submission & comments
+    submission.comments.replace_more(limit=0)
+    post["submission"] = RedditObject(submission.score,
+                                      submission.id,
+                                      submission.author.name,
+                                      submission.subreddit.name,
+                                      body=submission.title,
+                                      isSubmission=True)
 
-    # Works for text based reply subs (r/askreddit, r/askmen, etc...)
-    for n, submission in enumerate(submissions):
-        submission.comments.replace_more(limit=0)
-        print(n+1, submission.title)
-        f.write("TITLE\n")
-        f.write(submission.title + "\n")
-        f.write("COMMENTS\n")
-        comment_forest = submission.comments
-        for surface_comment in comment_forest[:5]:  # get top 5 comments
-            print("*"*8)
-            print(surface_comment.body)
-            f.write(">" + surface_comment.body + "\n")
-            for reply in surface_comment.replies:  # get 2 replies if their scores meet criteria
-                if reply.score >= (surface_comment.score / 2):
-                    print(reply.body)
-                    f.write("\t>"+reply.body+"\n")
-f.close()
+    print("Getting comments...")
+    comment_forest = submission.comments
+    gathered_text = str()
+    for surface_comment in comment_forest[:10]:  # get top x comments
+        if surface_comment.author is None:
+            safe_author_name = "deleted"
+        else:
+            safe_author_name = surface_comment.author.name
+
+        post["comments"].append(RedditObject(
+            surface_comment.score,
+            surface_comment.id,
+            authorName=safe_author_name,
+            subreddit=surface_comment.subreddit.name,
+            body=surface_comment.body,
+            isSubmission=False,
+        ))
+        gathered_text += surface_comment.body
+        # for .replies to get replies to comments
+    gathered_content.append(post)
+    print(len(post["comments"]), "gathered from current post...")
+
+pprint(gathered_content)
