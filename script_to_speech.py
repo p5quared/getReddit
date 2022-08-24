e4a1e5ab43ada0f5be46bb8313436a50bc29c1da
p5quared <petervaiciulis@gmail.com>
2022-08-24 17:44:45 -0400

    -minor notation add

--- script_to_speech.py
+++ script_to_speech.py
@@ -6,4 +6,5 @@ def script_to_speech(post, t_dir, language="en"):
     text_to_convert = [post.head.body] + [comment.body for comment in post.comments]
     for i, text in enumerate(text_to_convert):
         audio_obj = gTTS(text=text, lang=language, slow=False)
-        audio_obj.save("./test_resources/" + t_dir + "/" + str(i) + ".mp3")
+        audio_obj.save("./test_resources/" + t_dir + "/audio/" + str(i) + ".mp3")
+        #  ie [./test_resources/u1512/audio/{n}.mp3]
