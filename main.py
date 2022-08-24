bb998ca6d75c55dd8c3c5becfa5f28ab97c906d6
p5quared <petervaiciulis@gmail.com>
2022-08-24 17:45:14 -0400

    -rewrote main to attempt ot create 3 videos

--- main.py
+++ main.py
@@ -3,19 +3,25 @@ import os
 from getReddit import getReddit
 from image_maker import drawPost
 from script_to_speech import script_to_speech
+from movie_maker import makeMovie
 
 if __name__ == '__main__':
     print("GETTING TEST POSTS:")
     testPosts = getReddit('askreddit', 3)
-    one_test = testPosts[0]
-    test_path = os.path.join("./test_resources", one_test.id)
-    # make images
-    print("Generating images...")
-    drawPost(one_test.head, one_test.head.id, 0)
-    for i, post in enumerate(one_test.comments):
-        drawPost(post, one_test.head.id, i+1)
-    print("Images created!")
-    # create audio
-    print("Generating audio...")
-    script_to_speech(one_test, one_test.head.id)
-    print("Audio created!")
+    for post in testPosts:
+        test_path = os.path.join("./test_resources", post.id)
+        os.mkdir(test_path)
+        # make images
+        print("Generating images...")
+        os.mkdir("./test_resources/" + post.id + "/images/")
+        drawPost(post.head, post.head.id, 0)
+        for i, comment in enumerate(post.comments):
+            drawPost(comment, post.id, i+1)
+        print("Images created!")
+        # create audio
+        print("Generating audio...")
+        os.mkdir("./test_resources/" + post.id + "/audio/")
+        script_to_speech(post, post.id)
+        print("Audio created!")
+        print("Generating movie...")
+        makeMovie(post)
