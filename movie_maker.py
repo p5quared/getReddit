2eef875f82d0dabb545ed44cea8ab043b15d0ab0
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2022-09-03 23:50:03 -0400

    changed output path and extracted background video maker function.
    also changed to grab random portions of sample video

--- movie_maker.py
+++ movie_maker.py
@@ -1,10 +1,21 @@
 from moviepy.editor import *
+from random import randint
 import os
+
+
+def formatBackgroundVideo(target_duration):
+    bg = VideoFileClip("./background_clips/minecraft_bg1.mp4") \
+        .resize(height=1920) \
+        .crop(x1=1166.6, y1=0, x2=2246.6, y2=1920)
+
+    random_in_point = randint(0, int(bg.duration) - (int(target_duration) + 3))
+    bg_trimmed = bg.subclip(random_in_point, random_in_point + int(target_duration))
+    return bg_trimmed
 
 
 def makeMovie(post_object):
     # While it might make sense to just past a directory or id instead of the whole object,
-    # I feel this is more versatile for future implementations
+    # I feel this will be more versatile for future implementations
     base_directory = f'./test_resources/{post_object.id}/'
     audio_directory = base_directory + "audio/"
     image_directory = base_directory + "images/"
@@ -17,7 +28,7 @@ def makeMovie(post_object):
 
     joined_audio = concatenate_audioclips(audio_clips)
 
-    # clips
+    # image clips
     image_clips = []
     for filename in sorted(os.listdir(image_directory)):
         print(f'Getting image from {image_directory + filename}...')
@@ -26,22 +37,16 @@ def makeMovie(post_object):
     joined_clips = concatenate(timed_image_clips, method="compose")
 
     # background
-    bg = VideoFileClip("./test_resources/ocean_waves_bg.mp4").rotate(90)
-    bg_looped = bg.loop(duration=joined_audio.duration)
+    background_movie = formatBackgroundVideo(joined_clips.duration)
 
     # combine
-    sub_final = CompositeVideoClip([bg_looped, joined_clips.set_position((65, 600))])
+    sub_final = CompositeVideoClip([background_movie, joined_clips.set_position((65, 600))])
     final = sub_final.set_audio(joined_audio)
 
     # Audio does not work in quicktime... but it is there. Play in VLC to hear
-    final.write_videofile(base_directory + "out.mp4")
+    final.write_videofile("./output/" + post_object.id + "_out.mp4")
 
 
 if __name__ == '__main__':
-    class TestObject:
-        def __init__(self, test_id):
-            self.id = test_id
-
-
-    t = TestObject("wxbekm")
-    makeMovie(t)
+    test_background = formatBackgroundVideo(5)
+    test_background.write_videofile("./output/test_background.mp4")
