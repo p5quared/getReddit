d1730aa3dac4a726b285ce31a8b79daec4321d70
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2022-09-09 15:06:08 -0400

    added following to our class: -dir management -drawPost -script_to_speech

--- classes.py
+++ classes.py
@@ -1,5 +1,12 @@
 from dataclasses import dataclass
 from gtts import gTTS
+import os
+from shutil import rmtree
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
+
+username_color = (0, 0, 0)
+bg_color = (245, 245, 245)
 
 
 @dataclass(frozen=True, order=True)
@@ -23,3 +30,56 @@ class Post:
         self.head = head
         self.id = head.id
         self.comments = list()
+
+    def script_to_speech(self):
+        text_to_convert = [self.head.body] + [comment.body for comment in self.comments]
+        for i, text in enumerate(text_to_convert):
+            audio_obj = gTTS(text=text, lang="en", slow=False)
+            audio_obj.save("./working/" + self.id + "/sound/" + str(i) + ".mp3")
+            #  ie [./test_resources/u1512/audio/{n}.mp3]
+
+    def dirs_make(self):
+        working_directory = os.path.join("./working", self.id)
+        image = os.path.join(working_directory, 'image')
+        sound = os.path.join(working_directory, 'sound')
+        if os.path.exists(working_directory):
+            rmtree(working_directory)
+        os.mkdir(working_directory)
+        os.mkdir(image)
+        os.mkdir(sound)
+
+    def dirs_cleanUp(self):
+        working_directory = os.path.join("./working", self.id)
+        if os.path.exists(working_directory):
+            rmtree(working_directory)
+        else:
+            print(f"ERROR: attempted to delete <{working_directory}> which does not exist.")
+
+    def drawPost(self):
+        for i, post_object in enumerate([self.head] + self.comments):
+            body_wrapped = textwrap.wrap(post_object.body, 40)  # 2nd arg is a width limiter
+            body_string = "\n".join(body_wrapped)
+
+            # size canvas
+            if len(body_wrapped) == 1:
+                height = 48 * 4  # this just works out in my samples for one line comments
+            else:
+                height = (len(body_wrapped) + 3) * 48  # roughly height of wrapped-text plus 2 lines
+
+            # Pillow Stuff
+            out = Image.new('RGB', (950, height), color=bg_color)
+            d = ImageDraw.Draw(out)
+
+            poster = "u/" + post_object.authorName + ":"
+            if post_object.isSubmission:
+                poster = "Posted by: " + poster
+            body_x = 25
+            # if post_object.isReply:  # pushes body over and adds reply line
+            #     d.line((10, 120, 10, out.size[1] * .9), fill=(85, 89, 92), width=4)
+            #     body_x = + 25
+            #     poster = "Reply by: " + poster.authorName
+
+            font = ImageFont.truetype("Helvetica.ttc", 48)
+            d.text((25, 50), poster, font=font, fill=username_color)  # draw username
+            d.multiline_text((body_x, 120), body_string, font=font, fill=(0, 0, 0))  # draw comment text
+            out.save("./working/" + self.id + "/image/" + str(i) + ".jpeg")
--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-from _dirs import dirs_make, dirs_cleanUp
-from _script_to_speech import script_to_speech
 from getReddit import getReddit
-from image_maker import drawPost
 from movie_maker import makeMovie
 
 if __name__ == '__main__':
     print("GETTING TEST POSTS:")
     testPosts = getReddit('askreddit', 1)
     for post in testPosts:
-        dirs_make(post)
+        post.dirs_make()
+
         # make images
         print("Generating images...")
-        drawPost(post.head, post.id, 0)
-        for i, comment in enumerate(post.comments):
-            drawPost(comment, post.id, i+1)
+        post.drawPost()
         print("Images created!")
 
         # create audio
         print("Generating audio...")
-
-        script_to_speech(post, post.id)
+        post.script_to_speech()
         print("Audio created!")
 
         print("Generating movie...")
         makeMovie(post)
-
         print("Success!! \n Cleaning up...")
-        dirs_cleanUp(post)
+
+        post.dirs_cleanUp()
