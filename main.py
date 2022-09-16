8294dadc47c332e78e9a3d5d68b55d5381f30ebb
Peter Vaiciulis <98245483+p5quared@users.noreply.github.com>
2022-09-16 15:22:33 -0400

    -parameter to getReddit to take num_posts & comments_per_post

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from getReddit import getReddit
 
 if __name__ == '__main__':
     print("GETTING TEST POSTS:")
-    testPosts = getReddit('askreddit', num_posts=3, comments_per_post=3)
+    testPosts = getReddit('askmen', num_posts=1, comments_per_post=10)
     for post in testPosts:
         post.write_movie()
