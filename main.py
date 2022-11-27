77eb4aa42bfbdafd49b8bdbb2aadc22dc4075ce0
Peter V <98245483+p5quared@users.noreply.github.com>
2022-11-26 21:56:39 -0500

    posts are now storeable as JSON

--- main.py
+++ main.py
@@ -10,23 +10,29 @@ if __name__ == '__main__':
                          "3. Gather new objects.\n"
                          ">> "))
     print("GETTING TEST POSTS:")
-    if use_case == 2:  # run with gathered objects... parse from text files
+    if use_case == 2:  # Test Run
         for filename in os.listdir("./test_resources/sample_objects/"):
             with open(filename, "r") as json_file:
                 reddit_obj = json.load(json_file)
                 # problem : this does not return a reddit_obj, just a json_obj.
                 # will need to create a constructor to use this with...
+    print("Let's setup your sample...")
+    post_num = 1
+    comm_num = 5
+    content = getReddit('askreddit', post_num, comm_num)
+    if use_case == 3:  # Gather Objects
+        for post in content:  # Post Object
+            all_post_content = {"head": post.head.__dict__,
+                                "comments": [comment.__dict__ for comment in post.comments]}
+            with open("./test_resources/sample_objects/"+str(post.head.body), 'w') as f:
+                json.dump(all_post_content, f, indent=2)
+    if use_case == 1:
+        for post in content:
+            post.write_movie()
+    elif use_case == 3:
+        # Saving new json object
+        for i, post in enumerate(content):
+            post.write_test_object(i)
+        print("Objects written!")
     else:
-        print("Let's setup your sample...")
-        post_num = int(input("How many posts to grab?\n>> "))
-        comm_num = int(input("How many comments to grab?\n>> "))
-        content = getReddit('askreddit', post_num, comm_num)
-        if use_case == 1:
-            for post in content:
-                post.write_movie()
-        elif use_case == 3:
-            for i, post in enumerate(content):
-                post.write_test_object(i)
-            print("Objects written!")
-        else:
-            print("Input not understood.")
+        print("Input not understood.")
